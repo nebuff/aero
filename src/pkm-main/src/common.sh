813912ed6a8f95f2de203341947345e976143027
nebuff <bitfruit8@gmail.com>
2025-07-03 17:11:06 -0400

    Add files via upload

---
+++ src/pkm-main/src/common.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+hash() { builtin hash "$1" 2>/dev/null; }
+
+init_term() {
+    shopt -s checkwinsize; (:;:); ((rows=LINES - 1))
+
+    printf '\e[?1049h\e[?7l\e[?25l'
+}
+
+deinit_term() { printf '\e[?1049l\e[?7h\e[?25h'; }
+
+read_keys() { read -rsn1; [[ $REPLY == $'\e' ]] && read -rsn2; }
+
+_draw() {
+    unset i cursorHist
+    cursor="$rows"
+    
+    printf '\e[2J\e[%dH' "$LINES"
+    printf '%s\n' "$@"
+}
+
+hover_interface() {
+    local -n foo=$1
+    local fooInit=("$@") fooCnt="${#foo[@]}"
+    
+    : "${foo[cursor-LINES]%% - *}"
+    printf '\e[%dH\e[30;42m%s\e[m' "$cursor" "${_%%: *}"
+    
+    cursorHist+=("$cursor")
+    if (( ${i:=0} )); then
+        printf '\e[%dH%s' "${cursorHist[0]}" "${foo[cursorHist[0]-LINES]}"
+        cursorHist=("${cursorHist[@]:1}")
+    else
+        ((i++))
+    fi
+
+    read_keys
+    case $REPLY in
+        [qQ]) exit;;
+        '[B'|[kKsS]) ((cursor++));;
+        '[A'|[jJwW]) ((cursor--));;
+    esac
+
+    if (( ${#fooInit[@]} > rows )); then
+        (( cursor < LINES-fooCnt )) && cursor="$rows"
+
+        if (( cursor > rows )); then
+            fooInit=("${fooInit[@]:1}")
+            foo=("${fooInit[@]:0:${#foo[@]}+rows}")
+            _draw "${foo[@]}"
+            cursor=1
+        elif (( cursor < 1 )); then
+            fooInit=("${fooInit[@]:1}")
+            foo=("${foo[@]:0:${#foo[@]}-rows}")
+            _draw "${foo[@]}"
+        fi
+    else
+        if (( cursor > rows )); then
+            ((cursor=LINES-fooCnt))
+        elif (( cursor < LINES-fooCnt )); then
+            cursor="$rows"
+        fi
+    fi
+}
+
+base_keymap() {
+    for((;;)) {
+        _ibar '[<] back'
+
+        read_keys
+        [[ $REPLY =~ ^'[D'$|^[hHaA]$ ]] && return
+    }
+}
+
+_ibar() {
+    printf '\e[%dHPKM %s' "$LINES" "$1"
+    
+    [[ $2 ]] && {
+        printf '\e[?25h'
+
+        read -r "$2"
+
+        printf '\e[?25l'
+    }
+}
